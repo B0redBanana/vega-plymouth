0bfe16afac0a3c750832389d297a86ad78938b80
B0redBanana <53279592+B0redBanana@users.noreply.github.com>
2026-06-08 05:17:42 +0200

    v3.0.0 (#3)
    * v3.0.0 RC1
    
    * Updated README
    * Overhauled install.sh
    
    * Added option for Fedora
    * Improved handling of wrong or missing inputs
    * Added support for dracut
    
    * v.3.0.0 RC2 (The Cleanup)

--- README.md
+++ README.md
@@ -6,72 +6,101 @@
 </p>
 
 <p align="center">
-A plymouth animation, derived from <a href="https://vega-gaming.de/">VEGA's</a> logo.
+A Plymouth boot splash theme using <a href="https://steamcommunity.com/groups/VEGA-Gaming-Group">VEGA's</a> logo.
+<strong>Preview</strong><br>
 
-![gif](https://github.com/B0redBanana/vega-plymouth/blob/8697b7d5202bf335ef6f1677ece687d5e6515028/preview/logo.gif)
+<img src="https://github.com/B0redBanana/vega-plymouth/blob/8697b7d5202bf335ef6f1677ece687d5e6515028/preview/logo.gif" alt="preview gif">
+
 </p>
 
-### What is plymouth?
+## What is Plymouth?
 
 [Plymouth](http://www.freedesktop.org/wiki/Software/Plymouth) is a project from Fedora and now listed among the [freedesktop.org's official resources](https://www.freedesktop.org/wiki/Software/#graphicsdriverswindowsystemsandsupportinglibraries) providing a flicker-free graphical boot process. It relies on [kernel mode setting](https://wiki.archlinux.org/index.php/Kernel_mode_setting) (KMS) to set the native resolution of the display as early as possible, then provides an eye-candy splash screen leading all the way up to the login manager.
 
-### How to set it up?
+## Prerequisites
 
-[Click here](https://wiki.archlinux.org/index.php/plymouth) for an article on the Archwiki and [here](https://wiki.ubuntu.com/Plymouth) for the Ubuntuwiki.
+To use the theme, you need to have Plymouth installed and configured on your system. For help with that, please refer to your distribution's wiki.
 
-### How to get these themes?
+- [ArchWiki: Plymouth](https://wiki.archlinux.org/title/Plymouth) 
 
-**Download and extract** 
+- [Ubuntu Wiki: Plymouth](https://wiki.ubuntu.com/Plymouth)
 
-**or clone using:**
+Fedora users can also refer to the ArchWiki.
+
+## Download
+
+Download manually [here](https://github.com/B0redBanana/vega-plymouth-theme/archive/refs/heads/main.zip) and extract the archive.
+
+or clone using:
+
 ```bash
-git clone https://github.com/B0redBanana/vega-plymouth.git
+git clone https://github.com/B0redBanana/vega-plymouth-theme.git
 ```
 
-### How to use these themes?
+## Installation
 
-**Install using the script** 
-Once it's downloaded, run the install script and follow the instructions. It'll ask you for the path, your distro and preferred size!
+### Using the Installation Script
 
-**Install manually** (We're using the small version in this case, replace small with either medium or large, if desired)
+1. Navigate to the downloaded directory:
+   
+   ```bash
+   cd vega-plymouth-theme
+   ```
 
-+ For **Arch-based** distros
+2. Run the installation script
+   
+   ```bash
+   ./install.sh
+   ```
+
+3. Follow the on-screen instructions
+
+### Manual installation
+
+This example is using the small version; replace "small" with "medium" or "large" if wanted:
+
+1. Change into the directory containing the themes
+
 ```bash
-# Make sure Plymouth is installed and set up
-
-#cd inside the folder containing the themes
 cd vega-plymouth/vega
-
-# Copying the theme into Plymouth's folder
-sudo cp -r vega-small /usr/share/plymouth/themes/
-
-# Set the theme as default
-sudo plymouth-set-default-theme -R vega-plymouth-small
-
 ```
-+ For **Debian** based distros
+
+2. Copy the desired theme into the Plymouth folder
+
 ```bash
-# Make sure Plymouth is installed and set up
-
-#cd inside the folder containing the themes
-cd vega-plymouth/vega
-
-# Copying the theme into Plymouth's folder
 sudo cp -r vega-small /usr/share/plymouth/themes/
+```
 
-# Install the theme
+3. Set the theme as your default:
+
+For *Arch Linux & Fedora*: 
+
+```bash
+sudo plymouth-set-default-theme -R vega-small
+```
+
+For *Debian & Ubuntu*: 
+
+```bash
 sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/vega-small/vega-small.plymouth 100
 
-# Choose the theme you want to use
 sudo update-alternatives --config default.plymouth
+```
 
-# Then update initramfs
+4. Update your initramfs (use the one for your system):
+
+```bash 
+sudo mkinitcpio -P
+```
+```bash 
 sudo update-initramfs -u
-``` 
+```
+```bash 
+sudo dracut -f
+```
 
+## Important Notes
 
-
-### Important info
-+ This was created with a resolution of 1920x1080
-+ Feel free to leave suggestions or report any issues!
-+ I can't get this to work with an ecrypted drive at the moment! Help would be appreciated!
++ This was created with resolution of 1920x1080
++ Since this is my first Bash script, feedback is welcome.
++ I have not tested this on a LUKS encrypted drive yet
--- install.sh
+++ install.sh
@@ -1,99 +1,173 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Time to start the installer!
+
 clear
 
-echo "Enter Plymouth install location"
-read -e -p "Default - /usr/share/Plymouth/: " location
-if test -z "$location";
-then
-    location="/usr/share/Plymouth/"
-fi
-if test "${location: -1}" != "/"
-then
-    location="$location/"
+## Checking for elevated privileges
+
+if [ "$EUID" -ne 0 ]; then
+    echo
+    echo "This script requires sudo privileges. Restarting with sudo..."
+    sudo "$0" "$@"
+    exit
 fi
 
-DIR=$(pwd)
+dir="$(pwd)"
 
-read -p "1: Arch, 2: Debian: " DISTRO
+#################
+### FUNCTIONS ###
+#################
+
+                create_new_initramfs() {
+                    echo "Refreshing Initramfs"
+                    
+                    if command -v update-initramfs &>/dev/null; then
+                            update-initramfs -u &>/dev/null  
+                            elif command -v mkinitcpio &>/dev/null; then
+                            mkinitcpio -P &>/dev/null
+                            elif command -v dracut &>/dev/null; then
+                            dracut -f &>/dev/null
+                            else
+                        echo "Warning: Could not rebuild initramfs!"
+                    fi
+                }
+
+                install_theme() {
+                echo "Installing $theme"
+
+                if [[ ! -d "$dir/vega/$theme" ]]; then
+                echo "Theme directory '$theme' not found."
+                exit 1
+                fi
+
+                echo "Installing $theme to /usr/share/plymouth/themes/$theme"
+                cp -r "$dir/vega/$theme" "/usr/share/plymouth/themes/$theme"
+                }
+
+echo "================================="
+echo "  VEGA Plymouth Theme Installer  "
+echo "================================="
+echo
+
+echo "Please choose your Distribution:"
+echo "1) Arch"
+echo "2) Debian"
+echo "3) Ubuntu"
+echo "4) Fedora"
+echo
+
+### Getting Distro ###
+
+read -rp "Select [1-4]: " DISTRO
+
+case "$DISTRO" in
+    1|2|3|4) ;;
+    *)
+        echo "Invalid selection"
+        exit 1
+        ;;
+esac
+
+### Getting preferred size ###
+
+echo "Choose a Resolution:"
+echo "1) Small  (270x270px)"
+echo "2) Medium (540x540px)"
+echo "3) Large  (1080x1080px)"
+echo
+
+read -rp "Select [1-3] (default 1): " size_select
+
+size_select=${size_select:-1}
+
+case "$size_select" in
+    1)
+        theme="vega-small"
+        ;;
+    2)
+        theme="vega-medium"
+        ;;
+    3)
+        theme="vega-large"
+        ;;
+    *)
+        echo "Invalid selection. Defaulting to Small (270x270)"
+        theme="vega-small"
+        ;;
+esac
+
+### Starting Installation ###
 
 case $DISTRO in
 
+##############
+###  Arch  ###
+##############
+
         1)
-            echo "Pick A Size to Install:"
-            read -p "1: Small (270x270px), 2: Medium (540x540px), 3: Large (1080x1080px): " size_select
-            if test -z "$size_select";
-            then
-                size_select=1
-            fi
-            case "$size_select" in
-            1)
-                echo "Installing Small Version"
-                cd $DIR/vega
-                sudo cp -r vega-small /usr/share/plymouth/themes/
-                sudo plymouth-set-default-theme -R vega-small
-                ;;
-            2)
-                echo "Installing Medium Version"
-                cd $DIR/vega
-                sudo cp -r vega-medium /usr/share/plymouth/themes/
-                sudo plymouth-set-default-theme -R vega-medium
-                ;;
-            3)
-                echo "Installing Large Version"
-                cd $DIR/vega
-                sudo cp -r vega-large /usr/share/plymouth/themes/
-                sudo plymouth-set-default-theme -R vega-large
-                ;;
-            esac
-            ;;
+
+            install_theme
+            plymouth-set-default-theme -R "$theme"
+            echo
+            echo "Setting Theme as Default"
+            echo
+
+create_new_initramfs
+;;
+
+##############
+### DEBIAN ###
+##############
 
         2)
 
-            echo "Pick a size to install:"
-            read -p "1: Small (270x270px), 2: Medium (540x540px), 3: Large (1080x1080px): " size_select
-            if test -z "$size_select";
-            then
-                size_select=1
-            fi
+            install_theme
+            update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/"$theme"/"$theme".plymouth 100
+            echo "Running update-alternatives"
+            echo
+            echo "Please choose the number corresponding to the installed theme."
+            echo 
+            update-alternatives --config default.plymouth
 
-            case "$size_select" in
-                1)
-                    echo "Installing Small Version"
-                    cd $DIR/vega
-                    sudo cp -r vega-small /usr/share/plymouth/themes/
-                    sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/vega-small/vega-small.plymouth 100
-                    ;;
-                2)
-                    echo "Installing Medium Version"
-                    cd $DIR/vega
-                    sudo cp -r vega-medium /usr/share/plymouth/themes/
-                    sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/vega-medium/vega-medium.plymouth 100
-                    ;;
-                3)
-                    echo "Installing Large Version"
-                    cd $DIR/vega
-                    sudo cp -r vega-large /usr/share/plymouth/themes/
-                    sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/vega-large/vega-large.plymouth 100
-                    ;;
-            esac
-            
-            
-                echo "Choose Theme"
-                sudo update-alternatives --config default.plymouth
+create_new_initramfs
+;;
 
-                echo "Refreshing Initramfs"
-                create_new_initramfs() {
-                if type update-initramfs &>/dev/null; then
-                    update-initramfs -u &>/dev/null  
-                elif type mkinitcpio &>/dev/null; then
-                    mkinitcpio -P &>/dev/null
-                else
-                    echo "Warning: Could not rebuild initramfs!"
-                fi
-                }
+##############
+### Ubuntu ###
+##############
+
+        3)
+
+            install_theme
+            update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/"$theme"/"$theme".plymouth 100    
+            echo "Running update-alternatives"
+            echo                
+            echo "Please choose the number corresponding to the installed theme."
+            echo 
+            update-alternatives --config default.plymouth
+
+create_new_initramfs
+;;
+
+##############
+### Fedora ###
+##############
+
+        4)
+
+            install_theme
+            plymouth-set-default-theme "$theme" -R
+            echo
+            echo "Setting Theme as Default"
+            echo
+
+create_new_initramfs
+;;
 
 esac
 
+echo
 echo "Done!"
